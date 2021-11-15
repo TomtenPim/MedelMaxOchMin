0590246e4bf99a40f524cb19265638944e0a1226
TomtenPim <77719357+TomtenPim@users.noreply.github.com>
2021-11-15 11:20:05 +0100

    Wrote the rest

--- src/com/company/MedelMaxMin.java
+++ src/com/company/MedelMaxMin.java
@@ -6,12 +6,35 @@ import java.util.Scanner;
 public class MedelMaxMin {
 
     public static void main(String[] args) {
+
         int inputantal = Integer.parseInt(JOptionPane.showInputDialog("Hur många tal?" ));
 	    int [] input = new int[inputantal];
+
         Scanner tgb = new Scanner(System.in);
+
         for(int i = 0 ; i < input.length ; i++) {
-            System.out.println("Ange resultat " + (i+1) + ":");
+            System.out.println("Ange tal " + (i + 1) + ":");
             input[i] = tgb.nextInt();
         }
+
+            int summa = 0;
+            int max = 0;
+            int min = 0;
+
+            for(int i = 0; i < input.length; i++) {
+                if (max < input[i]);
+                max = input[i];
+            }
+
+            for (int i = 0; i < input.length; i++) {
+                if (min < input[i]);
+                min = input[i];
+            }
+
+            for (int i = 0; i < input.length; i++) {
+                summa += input[i];
+            }
+            int medel = summa/inputantal;
+            System.out.printf("Medeltalet är: " + medel + "n/ Högsta talet är: " + max + "n/ Minsta talet är: " + min);
     }
 }
